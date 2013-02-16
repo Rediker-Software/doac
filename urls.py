a6ce303186c7b3344ee550a942f17c7d80c2b6ae
Kevin Brown <bass5098@gmail.com>
2013-02-16 18:03:50 -0500

    Starting working on authorization

--- urls.py
+++ urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, include, url
 from . import views
 
 urlpatterns = patterns('',
+    url(r"^authorize/$", views.AuthorizeView.as_view(), name="oauth2_authorize"),
+    
     url(r"^redirect_endpoint/$", views.redirect_endpoint, name="oauth2_redirect_endpoint"),
 )
--- views.py
+++ views.py
@@ -1,4 +1,33 @@
 from django.http import HttpResponse
+from django.views.generic import View
+from . import exceptions
+
+
+class OAuthView(View):
+    
+    def check_get_parameters(self, *parameters):
+        for parameter in parameters:
+            if not self.request.GET.has_key(parameter):
+                return False
+        return True
+    
+    def render_exception(self, exception):
+        pass
+    
+    def redirect_exception(self, exeption):
+        pass
+
+
+class AuthorizeView(OAuthView):
+    
+    http_method_names = ("get", "post", )
+    
+    def get(self, request, *args, **kwargs):
+    
+        if self.check_get_parameters("client_id", "redirect_uri", "scope", "response_type"):
+            return HttpResponse()
+        else:
+            return HttpResponse("Error")
 
 
 def redirect_endpoint(request):
