62e4c80739fda8c79ada567cfb16e9fbb1ad6945
Kevin Brown <bass5098@gmail.com>
2013-02-16 15:59:37 -0500

    Added custom admin for clients

--- admin.py
+++ admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import AccessToken, AuthorizationToken, Client, RedirectUri, RefreshToken, Scope
 
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("name", "secret", "access_host", )
+    list_filter = ("is_active", )
+
 admin.site.register(AccessToken)
 admin.site.register(AuthorizationToken)
-admin.site.register(Client)
+admin.site.register(Client, ClientAdmin)
 admin.site.register(RedirectUri)
 admin.site.register(RefreshToken)
 admin.site.register(Scope)
