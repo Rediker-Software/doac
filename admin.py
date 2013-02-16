270cc4c10c9da6d0f072cb87cde31ac7d9bc69ff
Kevin Brown <bass5098@gmail.com>
2013-02-16 16:17:42 -0500

    Added admin for redirect uris

--- admin.py
+++ admin.py
@@ -6,9 +6,14 @@ class ClientAdmin(admin.ModelAdmin):
     list_display = ("name", "secret", "access_host", )
     list_filter = ("is_active", )
 
+
+class RedirectUriAdmin(admin.ModelAdmin):
+    list_display = ("client", "url", )
+
+
 admin.site.register(AccessToken)
 admin.site.register(AuthorizationToken)
 admin.site.register(Client, ClientAdmin)
-admin.site.register(RedirectUri)
+admin.site.register(RedirectUri, RedirectUriAdmin)
 admin.site.register(RefreshToken)
 admin.site.register(Scope)
