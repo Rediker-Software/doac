51ef14a0446bce25b61aa18e104c0ec9474f05d1
Kevin Brown <bass5098@gmail.com>
2013-02-16 15:59:18 -0500

    Added function for generating secret key for clients

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.db.models import signals
+from . import managers
 
 
 class AccessToken(models.Model):
@@ -29,14 +30,30 @@ class AuthorizationToken(models.Model):
 
 class Client(models.Model):
     name = models.CharField(max_length=255)
-    secret = models.CharField(max_length=255)
+    secret = models.CharField(max_length=50)
     access_host = models.URLField(max_length=255)
     is_active = models.BooleanField(default=True)
+    
+    def __unicode__(self):
+        return self.name
+    
+    def generate_secret(self):
+        from django.utils.crypto import get_random_string
+        
+        return get_random_string(50)
+        
+    def save(self, *args, **kwargs):
+        self.secret = self.generate_secret()
+        
+        super(Client, self).save(*args, **kwargs)
 
 
 class RedirectUri(models.Model):
     client = models.ForeignKey("Client", related_name="redirect_uris")
     url = models.URLField(max_length=255)
+    
+    class Meta:
+        verbose_name = "redirect URI"
 
 
 class RefreshToken(models.Model):
