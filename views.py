3c75c23e175829c1b675ae3fab23f048858c7a7f
Kevin Brown <bass5098@gmail.com>
2013-02-16 19:06:49 -0500

    Added function to verify the client

--- views.py
+++ views.py
@@ -12,9 +12,25 @@ class OAuthView(View):
         return True
     
     def render_exception(self, exception):
-        pass
+        return exception.http(exception.reason)
     
     def redirect_exception(self, exeption):
+        pass
+    
+    def verify_client(self):
+        from .models import Client
+        
+        if self.client_id:
+            try:
+                self.client = Client.objects.get(id=self.client_id)
+            except Client.DoesNotExist:
+                raise exceptions.ClientDoesNotExist()
+        else:
+            raise exceptions.ClientNotProvided()
+        
+    def verify_uri(self):
+        from urlparse import urlsplit
+        
         pass
 
 
@@ -23,11 +39,17 @@ class AuthorizeView(OAuthView):
     http_method_names = ("get", "post", )
     
     def get(self, request, *args, **kwargs):
-    
+        try:
+            self.client_id = request.GET.get("client_id", None)
+            self.verify_client()
+        except exceptions.InvalidRequest as e:
+            return self.render_exception(e)
+        
         if self.check_get_parameters("client_id", "redirect_uri", "scope", "response_type"):
             return HttpResponse()
         else:
-            return HttpResponse("Error")
+            if request.GET.has_key(""):
+                pass
 
 
 def redirect_endpoint(request):
