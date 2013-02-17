b98a1ff86ac700aa6b012d93e4ddfab6aa3f5e4a
Kevin Brown <bass5098@gmail.com>
2013-02-17 17:38:42 -0500

    Added JsonResponse for rendering json

--- oauth2_consumer/http.py
+++ oauth2_consumer/http.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+import json
 
 
 class HttpResponseUnauthorized(HttpResponse):
     status_code = 401
+
+
+class JsonResponse(HttpResponse):
+    
+    def __init__(self, data_dict, *args, **kwargs):
+        super(JsonResponse, self).__init__(json.dumps(data_dict), mimetype="text/json", *args, **kwargs)
