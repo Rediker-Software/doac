0170829460dc919b0a03939399aa15ea562a8d5d
Kevin Brown <bass5098@gmail.com>
2013-02-16 19:31:51 -0500

    Added exceptions for redirect URIs

--- exceptions.py
+++ exceptions.py
@@ -13,6 +13,14 @@ class ClientNotProvided(InvalidRequest):
 class ClientDoesNotExist(InvalidRequest):
     reason = "The client was malformed or invalid."
 
-    
+
+class RedirectUriNotProvided(InvalidRequest):
+    reason = "The redirect URI was malformed or invalid."
+
+
+class RedirectUriDoesNotValidate(InvalidRequest):
+    reason = "The reidrect URI does not validate against the client host."
+
+
 class InvalidScope(Exception):
     error = "invalid_scope"
