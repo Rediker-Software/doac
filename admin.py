ee073bddfa02e2d41b53b04775e429ccb8e806f7
Kevin Brown <bass5098@gmail.com>
2013-02-17 11:56:47 -0500

    Added the rest to the admin

--- admin.py
+++ admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import AccessToken, AuthorizationCode, AuthorizationToken, Client, RedirectUri, RefreshToken, Scope
 
 
+class AccessTokenAdmin(admin.ModelAdmin):
+    list_display = ("client", "user", "refresh_token", "token", )
+    list_filter = ("created_at", "expires_at", "is_active", )
+
+
 class AuthorizationCodeAdmin(admin.ModelAdmin):
     list_display = ("client", "redirect_uri", "token", )
     list_filter = ("created_at", "expires_at", "is_active", )
@@ -21,14 +26,19 @@ class RedirectUriAdmin(admin.ModelAdmin):
     list_display = ("client", "url", )
 
 
+class RefreshTokenAdmin(admin.ModelAdmin):
+    list_display = ("client", "user", "authorization_token", "token", )
+    list_filter = ("created_at", "expires_at", "is_active", )
+
+
 class ScopeAdmin(admin.ModelAdmin):
     list_display = ("short_name", "full_name", "description", )
 
 
-admin.site.register(AccessToken)
+admin.site.register(AccessToken, AccessTokenAdmin)
 admin.site.register(AuthorizationCode, AuthorizationCodeAdmin)
 admin.site.register(AuthorizationToken, AuthorizationTokenAdmin)
 admin.site.register(Client, ClientAdmin)
 admin.site.register(RedirectUri, RedirectUriAdmin)
-admin.site.register(RefreshToken)
+admin.site.register(RefreshToken, RefreshTokenAdmin)
 admin.site.register(Scope, ScopeAdmin)
