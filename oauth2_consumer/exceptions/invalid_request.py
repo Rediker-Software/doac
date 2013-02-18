b35e51a234f4149ba91a7753e14752ebcb5be9e9
Kevin Brown <bass5098@gmail.com>
2013-02-18 09:06:10 -0500

    Moved invalid_request exceptions to their own file

---
+++ oauth2_consumer/exceptions/invalid_request.py
@@ -0,0 +1,33 @@
+from .base import InvalidRequest
+
+
+class AuthorizationCodeAlreadyUsed(InvalidRequest):
+    reason = "The authorization code was already used to get a refresh token."
+
+
+class AuthorizationCodeNotProvided(InvalidRequest):
+    reason = "The authorization code was malformed or invalid."
+
+
+class AuthorizationCodeNotValid(InvalidRequest):
+    reason = "The authorization code was malformed or invalid."
+
+
+class ClientDoesNotExist(InvalidRequest):
+    reason = "The client was malformed or invalid."
+
+
+class ClientNotProvided(InvalidRequest):
+    reason = "The client was malformed or invalid"
+
+
+class RedirectUriDoesNotValidate(InvalidRequest):
+    reason = "The reidrect URI does not validate against the client host."
+
+
+class RedirectUriNotProvided(InvalidRequest):
+    reason = "The redirect URI was malformed or invalid."
+
+
+class ResponseTypeNotDefined(InvalidRequest):
+    reason = "The request type was malformed or invalid."
