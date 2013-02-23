7d6b7e5ddbeb73385f3d782b41f176504f91c598
Kevin Brown <bass5098@gmail.com>
2013-02-23 12:44:05 -0500

    Finished tests for the authorization code in approval view

--- tests/tests/views/approval.py
+++ tests/tests/views/approval.py
@@ -7,9 +7,28 @@ class TestApprovalErrors(ApprovalTestCase):
     def test_code(self):
         from oauth2_consumer.exceptions.invalid_request import AuthorizationCodeNotValid, AuthorizationCodeNotProvided
         
+        request = self.client.post(reverse("oauth2_approval"))
+        self.assertExceptionRendered(request, AuthorizationCodeNotProvided())
+        
+        request = self.client.post(reverse("oauth2_approval") + "?code=invalid")
+        self.assertExceptionRendered(request, AuthorizationCodeNotProvided())
+        
+        request = self.client.post(reverse("oauth2_approval"), {"code": "invalid"})
+        self.assertExceptionRendered(request, AuthorizationCodeNotValid())
+        
         data = {
             "code": "invalid",
         }
         
         request = self.client.post(reverse("oauth2_approval") + "?code=%s" % (self.authorization_code.token, ), data)
+        self.assertExceptionRendered(request, AuthorizationCodeNotValid())
+        
+        data = {
+            "code": self.authorization_code.token,
+        }
+        
+        request = self.client.post(reverse("oauth2_approval") + "?code=invalid", data)
+        self.assertExceptionRendered(request, AuthorizationCodeNotValid())
+        
+        request = self.client.post(reverse("oauth2_approval") + "?code=invalid", {"code": "invalid"})
         self.assertExceptionRendered(request, AuthorizationCodeNotValid())
