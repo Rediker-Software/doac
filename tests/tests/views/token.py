9e08487fa71e8189821778d817d341a9bd4cc4e0
Kevin Brown <bass5098@gmail.com>
2013-02-22 19:31:46 -0500

    Added test for grant_type with tokens

--- tests/tests/views/token.py
+++ tests/tests/views/token.py
@@ -1,1 +1,16 @@
 from ..test_cases import TokenTestCase
+
+
+class TestErrors(TokenTestCase):
+    
+    def test_grant_type(self):
+        from oauth2_consumer.exceptions.unsupported_grant_type import GrantTypeNotProvided, GrantTypeNotValid
+        
+        request = self.client.post("/oauth/token/")
+        self.assertExceptionJson(request, GrantTypeNotProvided())
+        
+        request = self.client.post("/oauth/token/", {"grant_type": ""})
+        self.assertExceptionJson(request, GrantTypeNotProvided())
+        
+        request = self.client.post("/oauth/token/", {"grant_type": "invalid"})
+        self.assertExceptionJson(request, GrantTypeNotValid())
