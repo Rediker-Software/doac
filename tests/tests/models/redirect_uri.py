267672413f9ba65b39659f9b92d5ff17968018d9
Kevin Brown <bass5098@gmail.com>
2013-02-24 13:49:24 -0500

    Added model tests for RedirectUri

---
+++ tests/tests/models/redirect_uri.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from oauth2_consumer.models import Client, RedirectUri
+
+
+class TestRedirectUriModel(TestCase):
+    
+    def test_unicode(self):
+        client = Client(name="Test Client", access_host="http://localhost/")
+        client.save()
+        
+        uri = RedirectUri(client=client, url="http://localhost/redirect_endpoint")
+        uri.save()
+        
+        self.assertEqual(unicode(uri), uri.url)
