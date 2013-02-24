7bb7a0d76259ddcc710676faf5addaf259f06a69
Kevin Brown <bass5098@gmail.com>
2013-02-24 17:20:39 -0500

    Added two more tests for the decorator

--- tests/tests/decorators.py
+++ tests/tests/decorators.py
@@ -1,10 +1,10 @@
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse
 from oauth2_consumer.decorators import scope_required
 from .test_cases import DecoratorTestCase
-from .mock import TestFunc
+
 
 class TestDecoratorErrors(DecoratorTestCase):
+
     def test_no_args(self):
         response = self.client.get(reverse("no_args"))
         
@@ -15,11 +15,30 @@ class TestDecoratorErrors(DecoratorTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, "success")
     
+    def test_has_scope(self):
+        response = self.client.get(reverse("has_scope"))
+        
+        self.assertEqual(response.status_code, 403)
+        
+        response = self.client.get(reverse("has_scope"), HTTP_AUTHORIZATION="Bearer %s" % (self.access_token.token, ))
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, "success")
+    
     def test_scope_doesnt_exist(self):
         response = self.client.get(reverse("scope_doesnt_exist"))
         
         self.assertEqual(response.status_code, 403)
         
         response = self.client.get(reverse("scope_doesnt_exist"), HTTP_AUTHORIZATION="Bearer %s" % (self.access_token.token, ))
+        
+        self.assertEqual(response.status_code, 403)
+    
+    def test_doesnt_have_all_scope(self):
+        response = self.client.get(reverse("doesnt_have_all_scope"))
+        
+        self.assertEqual(response.status_code, 403)
+        
+        response = self.client.get(reverse("doesnt_have_all_scope"), HTTP_AUTHORIZATION="Bearer %s" % (self.access_token.token, ))
         
         self.assertEqual(response.status_code, 403)
