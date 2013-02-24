602a0ae775d7425e9600350e93963a89191fc8f0
Kevin Brown <bass5098@gmail.com>
2013-02-24 11:00:21 -0500

    Added views for testing scopes

--- tests/urls.py
+++ tests/urls.py
@@ -6,4 +6,7 @@ urlpatterns = patterns('',
     url(r"^oauth/", include(oauth_urls)),
     
     url(r"^no_args/", views.no_args, name="no_args"),
+    url(r"^has_scope/", views.has_scope, name="has_scope"),
+    url(r"^scope_doesnt_exist/", views.scope_doesnt_exist, name="scope_doesnt_exist"),
+    url(r"^doesnt_have_all_scope/", views.doesnt_have_all_scope, name="doesnt_have_all_scope"),
 )
--- tests/views.py
+++ tests/views.py
@@ -4,3 +4,15 @@ from oauth2_consumer.decorators import scope_required
 @scope_required()
 def no_args(request):
     return HttpResponse("success")
+
+@scope_required("test")
+def has_scope(request):
+    return HttpResponse("success")
+
+@scope_required("invalid")
+def scope_doesnt_exist(request):
+    return HttpResponse("success")
+
+@scope_required("test", "invalid")
+def doesnt_have_all_scope(request):
+    return HttpResponse("success")
