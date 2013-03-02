e8b1952c84e6f72ea4887d1d4ceb4ebe6f667b05
Kevin Brown <bass5098@gmail.com>
2013-03-01 22:45:37 -0500

    Added compatibility for old urlconf

--- oauth2_consumer/urls.py
+++ oauth2_consumer/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls import patterns, include, url
+try:
+    from django.conf.urls import patterns, url
+except ImportError:
+    from django.conf.urls.defaults import patterns, url
+
 from . import views
 
 urlpatterns = patterns('',
