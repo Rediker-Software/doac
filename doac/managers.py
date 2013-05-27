1ad31bb60da3f471bdc212ee4810c0c8e3c8ca61
Kevin Brown <bass5098@gmail.com>
2013-05-27 19:07:01 -0400

    Finished added the skeletons for the managers

--- doac/managers.py
+++ doac/managers.py
@@ -25,3 +25,53 @@ class AccessTokenQuerySet(QuerySet):
     
     def is_active(self):
         return self.filter(active=True)
+
+
+class AuthorizationCodeManager(CustomManager):
+    
+    def get_query_set(self):
+        return AuthorizationCodeQuerySet(self.model)
+
+
+class AuthorizationCodeQuerySet(QuerySet):
+    pass
+
+
+class AuthorizationTokenManager(CustomManager):
+    pass
+
+
+class AuthorizationTokenQuerySet(QuerySet):
+    pass
+
+
+class ClientManager(CustomManager):
+    pass
+
+
+class ClientQuerySet(QuerySet):
+    pass
+
+
+class RedirectUriManager(CustomManager):
+    pass
+
+
+class RedirectUriQuerySet(QuerySet):
+    pass
+
+
+class RefreshTokenManager(CustomManager):
+    pass
+
+
+class RefreshTokenQuerySet(QuerySet):
+    pass
+
+
+class ScopeManager(CustomManager):
+    pass
+
+
+class ScopeQuerySet(QuerySet):
+    pass
