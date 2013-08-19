97f507ab5869c306ed468c683ca6e6e9b3266f5e
Kevin Brown <bass5098@gmail.com>
2013-08-19 13:39:54 -0400

    Split up the tests for AuthorizationToken

--- tests/tests/models/authorization_token.py
+++ tests/tests/models/authorization_token.py
@@ -8,35 +8,39 @@ class TestAuthorizationTokenModel(TestCase):
     def setUp(self):
         self.oclient = Client(name="Test Client", access_host="http://localhost/")
         self.oclient.save()
-        
+
         self.scope = Scope(short_name="test", full_name="Test Scope", description="Scope for testing")
         self.scope.save()
-        
+
         self.user = User(username="test", password="test", email="test@test.com")
         self.user.save()
-        
+
         self.token = AuthorizationToken(client=self.oclient, user=self.user)
         self.token.save()
-        
+
         self.token.scope = [self.scope]
         self.token.save()
 
     def test_unicode(self):
         self.assertEqual(unicode(self.token), self.token.token)
-        
-    def test_generate_refresh_token(self):
+
+    def test_generate_refresh_token_creates(self):
         rt = self.token.generate_refresh_token()
-        
+
         self.assertEqual(RefreshToken.objects.count(), 1)
         self.assertIsInstance(rt, RefreshToken)
-        
+
+    def test_generate_refresh_token_no_create_twice(self):
+        self.token.generate_refresh_token()
         rt = self.token.generate_refresh_token()
-        
+
         self.assertEqual(RefreshToken.objects.count(), 1)
         self.assertIsNone(rt)
-        
+
+    def test_generate_refresh_token_never_creates_twice(self):
+        self.token.generate_refresh_token()
         self.token.is_active = True
         rt = self.token.generate_refresh_token()
-        
+
         self.assertEqual(RefreshToken.objects.count(), 1)
         self.assertIsNone(rt)
